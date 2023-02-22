a509e576050430a2f1f0e21d6cc0d05851f59e87
Carlos <carlosblascoesapada@gmail.com>
2023-02-22 22:26:55 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ src/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_CBE"},{"p":"<Unnamed>","l":"FiguraGeometrica_CBE"},{"p":"<Unnamed>","l":"Rectangulo_CBE"},{"p":"<Unnamed>","l":"Triangulo_CBE"}];updateSearchResults();
